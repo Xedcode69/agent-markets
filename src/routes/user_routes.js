a1bfbcf48bf47a501018887afa045ee796e30dfa
xedcode <xedcode69@gmail.com>
2026-04-28 14:38:42 +0530

    add user routes

---
+++ src/routes/user_routes.js
@@ -0,0 +1,15 @@
+import {Router} from 'express'
+import {getAllUsersController, getBuyersController, getSellersController, getUserByIdController} from '../controllers/user_controller.js'
+import {authMiddleware} from '../middlewares/check_authorized.js'
+
+const userRoutes = Router();
+
+userRoutes.get('/all', getAllUsersController);
+
+userRoutes.get('/seller', getSellersController);
+
+userRoutes.get('/buyer', getBuyersController);
+
+userRoutes.get('/:id', {authMiddleware, getUserByIdController});
+
+export default userRoutes;
