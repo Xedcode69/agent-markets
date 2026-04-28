78eb1308fdf9bf94642b7bb9c5a6b0bc0429ff71
xedcode <xedcode69@gmail.com>
2026-04-28 14:34:26 +0530

    rename Router instance name in routes

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import {registerUser, loginUser} from "../controllers/auth_controller.js";
 import {verifyOTP, resendOTP} from "../controllers/otp_controller.js";
 
-const authRouter = Router();
+const authRoutes = Router();
 
-authRouter.post('/signup', registerUser);
-authRouter.post('/verify-otp', verifyOTP);
-authRouter.post('/resend-otp', resendOTP);
-authRouter.post('/login', loginUser);
+authRoutes.post('/signup', registerUser);
+authRoutes.post('/verify-otp', verifyOTP);
+authRoutes.post('/resend-otp', resendOTP);
+authRoutes.post('/login', loginUser);
 
-export default authRouter;
+export default authRoutes;
 
