e653d8b643193c22a607635e5879664c50ec7daa
xedcode <xedcode69@gmail.com>
2026-05-16 13:55:29 +0530

    fix: variable name mismatch

--- src/middleware/check_authorized.js
+++ src/middleware/check_authorized.js
@@ -11,7 +11,7 @@ const authMiddleware = (req, res, next)=> {
 
         const token = header.split(' ')[1];
 
-        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         req.user = decoded;
 
